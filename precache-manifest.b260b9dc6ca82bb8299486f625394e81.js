afa2020dffc935116051ecac09f5f59eb5b04cca
Oliver <oliveroenang@gmail.com>
2020-01-04 13:11:00 +0100

    Updates

---
+++ precache-manifest.b260b9dc6ca82bb8299486f625394e81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0778b0cbacbe8a210ecc1fed53ad08",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ca7b84bb28c4de47469b",
+    "url": "/weather-app/static/js/2.786c9a8c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/weather-app/static/js/2.786c9a8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4575a6f6b42edb19be9",
+    "url": "/weather-app/static/js/main.f004338f.chunk.js"
+  },
+  {
+    "revision": "df1ac26dc0e7b7d18b20",
+    "url": "/weather-app/static/js/runtime-main.a7aa11b4.js"
+  }
+]);
